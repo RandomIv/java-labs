9988953e41553fe6c3ad04d1eada03cf48917a1e
Alexander Ivanishin <ivanishin.alexander@lll.kpi.ua>
2025-09-16 21:10:41 +0300

    add lab7 solved

--- lab7/src/main/java/org/example/Main.java
+++ lab7/src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    static Predicate<String> areLettersAscending = word ->
+            IntStream.range(0, word.length() - 1)
+                    .allMatch(i -> word.charAt(i) < word.charAt(i + 1));
+    public static String[] splitLineToWords(String line){
+        return line.isBlank() ? new String[0] : line.split("\\s+");
+    }
+    public static List<String> handleLine(String line) {
+        return Arrays.stream(splitLineToWords(line)).filter(areLettersAscending).toList();
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        handleLine(line).forEach(word -> System.out.print(word + "\t"));
     }
 }
