c07979259b7c288a31dff3b319d093278de524e7
Alexander Ivanishin <ivanishin.alexander@lll.kpi.ua>
2025-10-27 05:07:04 +0200

    refactor lab7 solution

--- lab7/src/main/java/org/example/Main.java
+++ lab7/src/main/java/org/example/Main.java
@@ -3,22 +3,32 @@ package org.example;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
-    static Predicate<String> areLettersAscending = word ->
+
+    static final Predicate<String> areLettersAscending = word ->
             IntStream.range(0, word.length() - 1)
                     .allMatch(i -> word.charAt(i) < word.charAt(i + 1));
-    public static String[] splitLineToWords(String line){
-        return line.isBlank() ? new String[0] : line.split("\\s+");
-    }
-    public static List<String> handleLine(String line) {
-        return Arrays.stream(splitLineToWords(line)).filter(areLettersAscending).toList();
-    }
+
+    static final Function<String, Stream<String>> splitLineToStream = line ->
+            line.isBlank() ? Stream.empty() : Arrays.stream(line.split("\\s+"));
+
+    static final Function<String, List<String>> processLine =
+            splitLineToStream.andThen(stream -> stream.filter(areLettersAscending).toList());
+
+    static final Supplier<String> lineSupplier = new Scanner(System.in)::nextLine;
+
+    static final Consumer<List<String>> printWords = list ->
+            list.forEach(word -> System.out.print(word + "\t"));
+
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        handleLine(line).forEach(word -> System.out.print(word + "\t"));
+        printWords.accept(processLine.apply(lineSupplier.get()));
     }
 }
